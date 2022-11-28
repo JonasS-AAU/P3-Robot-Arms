8efbe69d37e23e0bd21470cb4f2d2ebc2a7964df
SvendLDS <90241159+SvendLDS@users.noreply.github.com>
2022-11-28 13:32:53 +0100

    fixed commands.py
    commands now execute instead of returning command bytearray

--- Dobot Contol Library com352/commands.py
+++ Dobot Contol Library com352/commands.py
@@ -4,60 +4,66 @@ import struct
 
 class Commands:
     def __init__(self,port):
-        coms.setup_serial(self,port)
+        self.serial_port = coms.setup_serial(self,port)
 
-def set_conveyor_speed(self,speed):
-    payload = coms.pack_payload(coms,'=BBBBi', 135,3,0,1,speed)
-    length = coms.calculate_length(coms,'=BBBBi')
-    checksum = coms.calculate_checksum(coms,list(payload))
-    print(checksum)
-    command = coms.build_command(coms,payload,length,checksum)
-    return command
+    def set_conveyor_speed(self,speed):
+        payload = coms.pack_payload(coms,'=BBBBi', 135,3,0,1,speed)
+        length = coms.calculate_length(coms,'=BBBBi')
+        checksum = coms.calculate_checksum(coms,list(payload))
+        print(checksum)
+        command = coms.build_command(coms,payload,length,checksum)
+        coms.send_command(command)
 
-def home(self): 
-    payload = coms.pack_payload(coms, '=BBi', 31, 3, 0)
-    length = coms.calculate_length(coms,'=BBi')
-    checksum = coms.calculate_checksum(coms,list(payload))
-    command = coms.build_command(coms,payload,length,checksum)
-    return command
 
-def enable_suction(self,enabled):
-    payload = coms.pack_payload(coms, '=BBBB', 62, 3, enabled, 1)
-    length = coms.calculate_length(coms, '=BBBB')
-    checksum = coms.calculate_checksum(coms,list(payload))
-    command = coms.build_command(coms,payload,length,checksum) 
-    return command
+        return command
 
-def move_arm(self,x,y,z):
-    payload = coms.pack_payload(coms, '=BBBffff',84,3,2,x,y,z,0)
-    length = coms.calculate_length(coms, '=BBBffff')
-    checksum = coms.calculate_checksum(coms, list(payload))
-    command = coms.build_command(coms,payload,length,checksum)
-    return command
+    def home(self): 
+        payload = coms.pack_payload(coms, '=BBi', 31, 3, 0)
+        length = coms.calculate_length(coms,'=BBi')
+        checksum = coms.calculate_checksum(coms,list(payload))
+        command = coms.build_command(coms,payload,length,checksum)
+        return command
 
-def get_pose(self):
-    payload = coms.pack_payload(coms,'=BB',10,0)
-    length = coms.calculate_length(coms, '=BB')
-    checksum = coms.calculate_checksum(coms, list(payload))
-    command = coms.build_command(coms,payload,length,checksum)
-    return command
+    def enable_suction(self,enabled):
+        payload = coms.pack_payload(coms, '=BBBB', 62, 3, enabled, 1)
+        length = coms.calculate_length(coms, '=BBBB')
+        checksum = coms.calculate_checksum(coms,list(payload))
+        command = coms.build_command(coms,payload,length,checksum) 
+        
+        coms.send_command(coms,self.serial_port,command)
+        return command
+    
 
-def read_pose(self):
-    ser.flush()
-    self.get_pose()
-    wrap_raw = ser.read(5)
-    wrapper = struct.unpack('=BBBBB',wrap_raw)
-    print(wrapper)
-    pose_raw = ser.read(20)
-    pose = struct.unpack('=fffff',pose_raw)
-    print(pose)
-    #rotation and joint angle not currently in use
-    (x,y,z,r,j_angle) = pose
-    return x,y,z
+    def move_arm(self,x,y,z):
+        payload = coms.pack_payload(coms, '=BBBffff',84,3,2,x,y,z,0)
+        length = coms.calculate_length(coms, '=BBBffff')
+        checksum = coms.calculate_checksum(coms, list(payload))
+        command = coms.build_command(coms,payload,length,checksum)
+        return command
+
+    def get_pose(self):
+        payload = coms.pack_payload(coms,'=BB',10,0)
+        length = coms.calculate_length(coms, '=BB')
+        checksum = coms.calculate_checksum(coms, list(payload))
+        command = coms.build_command(coms,payload,length,checksum)
+        return command
+
+    def read_pose(self):
+        ser.flush()
+        self.get_pose()
+        wrap_raw = ser.read(5)
+        wrapper = struct.unpack('=BBBBB',wrap_raw)
+        print(wrapper)
+        pose_raw = ser.read(20)
+        pose = struct.unpack('=fffff',pose_raw)
+        print(pose)
+        #rotation and joint angle not currently in use
+        (x,y,z,r,j_angle) = pose
+        return x,y,z
         
 
 
-cmds = Commands("COM4")
+cmds = Commands("COM3")
 
-    
+cmds.enable_suction(0)
     
