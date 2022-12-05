b970890b17f5d4a948376b775f8500450b8ea027
SvendLDS <90241159+SvendLDS@users.noreply.github.com>
2022-12-05 10:47:08 +0100

    created first_test.py

--- Dobot Contol Library com352/camera.py
+++ Dobot Contol Library com352/camera.py
@@ -6,17 +6,16 @@ import cv2
 
 class Camera:
     def __init__(self):
-        camera = PiCamera()
-        rawCapture = PiRGBArray(camera)
-        time.sleep(0.1)
+        pass
 
     def capture_image():
-                
+        camera = PiCamera()
+        rawCapture = PiRGBArray(camera)
+        time.sleep(0.1)      
         camera.capture(rawCapture,format='bgr')
         image = rawCapture.array
         return image
 
     def save_image(image,filename):
-        
         cv2.imwrite(filename,image)
 
